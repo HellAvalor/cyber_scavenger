62eed645e1a0ba781a14aafc29b7a41034bcfd53
Andrey Karaman <andreykaraman@together.com>
2014-10-16 18:31:03 +0300

    20141016 Add smooth moving

--- core/src/com/andrewkaraman/survival/core/actors/Player.java
+++ core/src/com/andrewkaraman/survival/core/actors/Player.java
@@ -17,8 +17,51 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 public class Player extends Actor {
 
     InputListener listener;
-
     Texture region;
+
+    boolean moveRight;
+
+    public boolean isMoveRight() {
+        return moveRight;
+    }
+
+    public void setMoveRight(boolean moveRight) {
+        if(moveLeft && moveRight) moveLeft = false;
+        this.moveRight = moveRight;
+    }
+
+    public boolean isMoveLeft() {
+        return moveLeft;
+    }
+
+    public void setMoveLeft(boolean moveLeft) {
+        if(moveLeft && moveRight) moveRight = false;
+        this.moveLeft = moveLeft;
+    }
+
+    public boolean isMoveUp() {
+        return moveUp;
+    }
+
+    public void setMoveUp(boolean moveUp) {
+        //todo
+        this.moveUp = moveUp;
+    }
+
+    public boolean isMoveDown() {
+        return moveDown;
+    }
+
+    public void setMoveDown(boolean moveDown) {
+        //todo
+        this.moveDown = moveDown;
+    }
+
+    boolean moveLeft;
+    boolean moveUp;
+    boolean moveDown;
+    float velocity;
+
     private final String LOG_CLASS_NAME = this.getClass().getName();
     private static final float MAX_MOVEMENT_SPEED = 25;
 
@@ -33,7 +76,8 @@ public class Player extends Actor {
     public void draw (Batch batch, float parentAlpha) {
         Color color = getColor();
         batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
-        batch.draw(region, getX(), getY());
+        batch.draw(region, getX(), getY(), getWidth()*5, getHeight()*5);
+
     }
 
     @Override
@@ -74,32 +118,90 @@ public class Player extends Actor {
 
             Gdx.app.log(LOG_CLASS_NAME, "keyDown " + Input.Keys.toString(keycode));
 
-            switch (keycode){
-                case Input.Keys.LEFT: {
-                    addAction(Actions.moveBy(-10,0,1));
-                    return false;
-                }
-                case Input.Keys.RIGHT: {
-                    addAction(Actions.moveBy(10, 0, 1));
-                    return false;
-                }
-                case Input.Keys.UP: {
-                    addAction(Actions.moveBy(0, 10, 1));
-                    return false;
-                }
-                case Input.Keys.DOWN: {
-                    addAction(Actions.moveBy(0, -10, 1));
-                    return false;
-                }
-                default:
-                    return false;
+//            switch (keycode){
+//                case Input.Keys.LEFT: {
+//                    addAction(Actions.moveBy(-20,0,5));
+//                    return false;
+//                }
+//                case Input.Keys.RIGHT: {
+//                    addAction(Actions.moveBy(20, 0, 5));
+//                    return false;
+//                }
+//                case Input.Keys.UP: {
+//                    addAction(Actions.moveBy(0, 20, 5));
+//                    return false;
+//                }
+//                case Input.Keys.DOWN: {
+//                    addAction(Actions.moveBy(0, -20, 5));
+//                    return false;
+//                }
+//                default:
+//                    return false;
+//            }
+            switch (keycode)
+            {
+                case Input.Keys.LEFT:
+                    setMoveLeft(true);
+                    break;
+
+                case Input.Keys.RIGHT:
+                    setMoveRight(true);
+                    break;
+
+                case Input.Keys.UP:
+                    setMoveUp(true);
+                    break;
+
+                case Input.Keys.DOWN:
+                    setMoveDown(true);
+                    break;
             }
+            return false;
         }
 
         @Override
         public boolean keyUp(InputEvent event, int keycode) {
             Gdx.app.log(LOG_CLASS_NAME, "keyup " + Input.Keys.toString(keycode));
+            switch (keycode)
+            {
+                case Input.Keys.LEFT:
+                    setMoveLeft(false);
+                    break;
+
+                case Input.Keys.RIGHT:
+                    setMoveRight(false);
+                    break;
+
+                case Input.Keys.UP:
+                    setMoveUp(false);
+                    break;
+
+                case Input.Keys.DOWN:
+                    setMoveDown(false);
+                    break;
+            }
             return false;
+        }
+    }
+
+    private void updateMotion()
+    {
+        if (isMoveLeft())
+        {
+            x -= 5 * Gdx.graphics.getDeltaTime();
+        }
+        if (isMoveRight())
+        {
+            x += 5 * Gdx.graphics.getDeltaTime();
+        }
+
+        if (isMoveUp())
+        {
+            y -= 5 * Gdx.graphics.getDeltaTime();
+        }
+        if (isMoveDown())
+        {
+            y += 5 * Gdx.graphics.getDeltaTime();
         }
     }
 }
