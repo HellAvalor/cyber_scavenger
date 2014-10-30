ba4afb860f2be9014c6878507748f538dd919614
Andrey Karaman <andreykaraman@together.com>
2014-10-30 21:13:52 +0200

    20141030 Add max speed limit

--- core/src/com/andrewkaraman/survival/core/actors/NewPlayer.java
+++ core/src/com/andrewkaraman/survival/core/actors/NewPlayer.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
@@ -26,12 +27,14 @@ public class NewPlayer extends Image {
     public final Body body; // newPlayer's box2d body
     private final int SHIP_WIDTH = 1;
     float DEG_TO_RAD =0.017453292519943295769236907684886f;
-
+    private final float MAX_SPEED = 10;
     private long shootingSpeed = 100000000;
     private long lastBulletTime;
     public float angle;
 
     boolean isShooting;
+
+    boolean rotating;
     boolean moveRight;
     boolean moveLeft;
     boolean moveUp;
@@ -52,22 +55,23 @@ public class NewPlayer extends Image {
 
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.x = 4f;
-        bodyDef.position.y = 4f;
+        bodyDef.position.x = 0;
+        bodyDef.position.y = 0;
         bodyDef.linearDamping = 0.1f;
         bodyDef.angularDamping = 0.1f;
         bodyDef.angle = angle;
 
+
         this.body = world.box2dWorld.createBody(bodyDef);
 //            this.body.setUserData(ElementType.BOB);
-
         BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("testPhysSettings.json"));
 
         FixtureDef fd = new FixtureDef();
         fd.friction = 0.1f;
         fd.restitution = 0.3f;
-        fd.density = 1;
+        fd.density = 2;
 
+        body.getMassData().center.set(SHIP_WIDTH/2, SHIP_WIDTH*(tex.getHeight()/tex.getWidth()) /2);
 //        Fixture fix = body.createFixture(circle, 50);
 //        fix.setDensity(1);
 //        fix.setFriction(1f);
@@ -111,35 +115,48 @@ public class NewPlayer extends Image {
             body.applyForceToCenter(0, -velocity, true);
         }
 
+        //TODO idea for max speed
+        Vector2 velocity = body.getLinearVelocity();
+        float speed = velocity.len();
+        if (speed > MAX_SPEED) {
+            body.setLinearVelocity(velocity.scl(MAX_SPEED / speed));
+        }
+
+        checkNeedRotating();
         updateAngle();
 //        Gdx.app.log(LOG_CLASS_NAME, "Player texture position at " + getX() + " / " + getY() +" body " + body.getPosition().x +" / "+ body.getPosition().y);
 
-        //TODO idea for max speed
-//        Vector2 velocity = body.getLinearVelocity();
-//        float speed = velocity.len();
-//        if (speed > MAX_SPEED) {
-//            body.setLinearVelocity(velocity.scl(MAX_SPEED / speed));
-//        }
+
     }
 
 
-    private void updateAngle(){
-
+    private void updateAngle() {
+        if (rotating) {
 //        angle = body.getLinearVelocity().x / (float) Math.sqrt(Math.pow(body.getLinearVelocity().x, 2) + (Math.pow(body.getLinearVelocity().y, 2)));
+//
+//        int y = 5;
+//        int x = 5;
+            float desiredAngle = (float) Math.atan2(-body.getLinearVelocity().x, body.getLinearVelocity().y);
+//        float desiredAngle = (float) Math.atan2( -y, x );
+            float nextAngle = body.getAngle() + body.getAngularVelocity() / 60f;
+            float totalRotation = desiredAngle - nextAngle;
+            while (totalRotation < -180 * DEG_TO_RAD) totalRotation += 360 * DEG_TO_RAD;
+            while (totalRotation > 180 * DEG_TO_RAD) totalRotation -= 360 * DEG_TO_RAD;
+            float desiredAngularVelocity = totalRotation * 60;
+            float change = 10 * DEG_TO_RAD; //allow 1 degree rotation per time step
+            desiredAngularVelocity = Math.min(change, Math.max(-change, desiredAngularVelocity));
+            float impulse = body.getAngularDamping() * desiredAngularVelocity;
+//        body.applyAngularImpulse(impulse, true);
+            body.setTransform(body.getPosition(), body.getAngle() + impulse);
+        }
+    }
 
-        int y = 5;
-        int x = 5;
-//        float desiredAngle = (float) Math.atan2( -body.getLinearVelocity().y, body.getLinearVelocity().x );
-        float desiredAngle = (float) Math.atan2( -y, x );
-        float nextAngle = body.getAngle() + body.getAngularVelocity() / 60f;
-        float totalRotation = desiredAngle - nextAngle;
-        while ( totalRotation < -180 * DEG_TO_RAD ) totalRotation += 360 * DEG_TO_RAD;
-        while ( totalRotation >  180 * DEG_TO_RAD ) totalRotation -= 360 * DEG_TO_RAD;
-        float desiredAngularVelocity = totalRotation * 60;
-        float change = 1 * DEG_TO_RAD; //allow 1 degree rotation per time step
-        desiredAngularVelocity = Math.min( change, Math.max(-change, desiredAngularVelocity));
-        float impulse = body.getAngularDamping() * desiredAngularVelocity;
-        body.applyAngularImpulse(impulse, true);
+    public boolean isRotating() {
+        return rotating;
+    }
+
+    public void setRotating(boolean rotating) {
+        this.rotating = rotating;
     }
 
     public boolean isMoveRight() {
@@ -205,6 +222,10 @@ public class NewPlayer extends Image {
 
     public void stop(){
         body.setLinearVelocity(0,0);
+    }
+
+    private void checkNeedRotating(){
+       setRotating(moveDown || moveLeft ||moveRight || moveUp);
     }
 }
 
